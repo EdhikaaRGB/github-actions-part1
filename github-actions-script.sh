79a3f2744b453edc87349ef9fbb0e5ef9952cebf
EdhikaaRGB <120156563+EdhikaaRGB@users.noreply.github.com>
2022-12-09 14:23:50 +0530

    Update github-actions-script.sh

--- github-actions-script.sh
+++ github-actions-script.sh
@@ -1,28 +1,30 @@
-          echo 1. What is in this directory?
-          ls -a
+echo 1. What is in the directory ?
+          ls -la 
           echo
-          echo 2. Is Java installed?
+          echo 2. Is java is installed ?
           java -version
           echo
-          echo 3. Is Git installed?
+          echo 3. Is Git installed ?
           git --version
-          echo 4. What build tools are installed?
+          echo
+          echo 4. what about the build tools ?
           mvn --version
-          gradle --version
           ant -version
+          gradle --version
           echo
-          echo 5. Where is the Android SDK Root?
-          echo $ANDROID_SDK_ROOT	
+          echo 5. Where is the Android SDK Root ?
+          echo $ANDROID_SDK_ROOT
           echo
-          echo 6. Where are the Selenium jars?
+          echo 6. Where is the selenium jars?
           echo $SELENIUM_JAR_PATH
-          echo
-          echo 7. What is the workspace location
+          echo9*-*
+          echo 7. What is the workspace location?
           echo $RUNNER_WORKSPACE
           echo
-          echo 8. Who is running this script?
+          echo 8. Who is running the script ?
           whoami
-          echo 9. How is the disc laid out?
+          echo 9. What is the disk layout?
           df
-          echo 10. What environment variables are avaialable?
-          env
+          echo
+          echo 10. What are the Environment VAriables?
+          env 
